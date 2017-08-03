cf81198f28947d1a3438f2b754ba04670d1db7c3
Giansalmi <gianmarco.salmistraro.1@studenti.unipd.it>
2017-08-03 15:38:21 +0200

    aggiunti link a modifica password e modifica mail dal menu

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { EditorContainerComponent } from './components/editor-container/editor-container.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { ModEmailComponent } from './components/editor-container/components/menu/components/profilo/components/mod-email/mod-email.component';
+import { ModPswComponent } from './components/editor-container/components/menu/components/profilo/components/mod-psw/mod-psw.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login',  component: LoginComponent },
   { path: 'registration',  component: RegistrationComponent },
   { path: 'editor', component: EditorContainerComponent },
+  { path: 'modemail', component: ModEmailComponent },
+  { path: 'modpsw', component: ModPswComponent }
 ];
 
 @NgModule({
