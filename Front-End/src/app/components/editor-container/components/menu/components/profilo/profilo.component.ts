679a2f70fd7d1121c6d031fed4dc57d32683870c
Firion711 <jacknapier.serious@gmail.com>
2017-08-05 23:15:01 +0200

    ADD commenti e identato

--- Front-End/src/app/components/editor-container/components/menu/components/profilo/profilo.component.ts
+++ Front-End/src/app/components/editor-container/components/menu/components/profilo/profilo.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-profilo',
   templateUrl: './profilo.component.html',
   styleUrls: ['./profilo.component.css']
 })
-export class ProfiloComponent implements OnInit {
-
-  constructor() { 
-
-  }
-
+export class ProfiloComponent {
   
-  ngOnInit() { }
-
-    ngAfterViewInit(){
-$('#menuProfilo').click(function(){
-  if (!$('#menuProfiloToggle').is(':visible'))
-    $('.tmp-disable').addClass('disabled');
-  else
-    $('.tmp-disable').removeClass('disabled');
-});
-
-       }
-
-
+  constructor() {}
+  
+  ngAfterViewInit(){
+    /**
+     * This directiove disable all the button in the top menu while the manage profile div is open
+     */
+    $('#menuProfilo').click(function(){
+      if (!$('#menuProfiloToggle').is(':visible'))
+        $('.tmp-disable').addClass('disabled');
+      else
+        $('.tmp-disable').removeClass('disabled');
+    });
+    
+  }
+  
+  
 }
