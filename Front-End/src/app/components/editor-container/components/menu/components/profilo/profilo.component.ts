1dc4ed6092f4f75573e4a813f0ecfd9d66f1083c
Firion711 <jacknapier.serious@gmail.com>
2017-08-09 11:16:17 +0200

    PROVA FIX 2
    (devo testare con sasà) sul mio schermo non ho questo problema

--- Front-End/src/app/components/editor-container/components/menu/components/profilo/profilo.component.ts
+++ Front-End/src/app/components/editor-container/components/menu/components/profilo/profilo.component.ts
@@ -15,16 +15,16 @@ export class ProfiloComponent {
     * This directiove disable all the button in the top menu while the manage profile div is open
     */
     $(window).resize(function(){
-      let y:number = -$("#qwerty").position().left;
-      $('#manage-profile').css({left: y});
-      $('#manage-profile').width($(window).width());
+      let y:number = -$("#qwerty").position().left + 1;
+      $('#manage-profile').css({ left: ( y ) });
+      $('#manage-profile').width($('.barra-menu').width() - 1);
     });
     $('#menuProfilo').click(function(){
       if (!$('#manage-profile').is(':visible')) {
         $('.tmp-disable').addClass('disabled');
-        $('#manage-profile').width($(window).width());
+        $('#manage-profile').width($('.barra-menu').width() - 1);
         let y:number = -$("#qwerty").position().left + 1;
-        $('#manage-profile').css({left: y});
+        $('#manage-profile').css({ left: ( y  ) });
       }
       else
         $('.tmp-disable').removeClass('disabled');
