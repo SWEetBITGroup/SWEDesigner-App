5441619ea1a5e3439c58b628500757bd5cf01f51
massi94 <massignanfab@gmail.com>
2017-08-13 17:11:40 +0200

    Finita configurazione di Karma per testing

--- Front-End/karma.conf.js
+++ Front-End/karma.conf.js
@@ -1,10 +1,17 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// Karma configuration
+// Generated on Sun Aug 13 2017 15:37:58 GMT+0200 (CEST)
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', '@angular/cli'],
+
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -15,12 +22,26 @@ module.exports = function (config) {
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+
+    // list of files / patterns to load in the browser
     files: [
-      { pattern: './src/test.ts', watched: false }
+	{ pattern: './test/test.ts', watched: false }
     ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+	'./test/test.ts': ['@angular/cli']
     },
+
+    //webpack: require('./webpack.config')({env: 'test'}),
+
     mime: {
       'text/x-typescript': ['ts','tsx']
     },
@@ -31,14 +52,43 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
               : ['progress', 'kjhtml'],
+
+
+    // web server port
     port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-    singleRun: false
-  });
-};
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
