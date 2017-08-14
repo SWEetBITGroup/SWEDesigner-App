babadca9751a8d5c9f6d287ff53e505d6496db5f
Firion711 <jacknapier.serious@gmail.com>
2017-08-14 21:55:34 +0200

    Ristrutturazione class menu parte attributi v. 0.2
    Ci son ancora cose da fixxare, ma intanto è stata stesa la struttura

--- Front-End/src/app/components/editor-container/components/editor/components/edit-class-menu/components/class-list-attribute/class-list-attribute.component.ts
+++ Front-End/src/app/components/editor-container/components/editor/components/edit-class-menu/components/class-list-attribute/class-list-attribute.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ClassMenuService } from '../../../../services/class-menu.service';
+import { MainEditorService } from '../../../../../../../../services/main-editor.service';
+import { ActivityService } from '../../../../services/activity.service'
+import { Subscription } from 'rxjs/Subscription';
+import { Classe } from '../../../../models/classe';
+
 @Component({
   selector: 'app-class-list-attribute',
   templateUrl: './class-list-attribute.component.html',
