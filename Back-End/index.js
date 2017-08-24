69a6a47bbfe8a2d626689bbe51f639cceae82a59
satowine1 <satowine@gmail.com>
2017-08-24 15:31:50 +0200

    Tolta bestemmia da un console.log

--- Back-End/index.js
+++ Back-End/index.js
@@ -380,11 +380,9 @@ app.listen(port, ()=>{
       })
     })
     app.post("/updateProj", function(req, res){
-      console.log("ENTRATA DIO CANE NELLA FUNZIONE DEL PORCO DIO. MADONNA LADRA.")
       var name = req.body.nome_progetto;
       var usr = req.body.username;
       var proj = req.body.project;
-      console.log("nome Proj: " + name + "username: " + usr + "body: " + proj);
       mongooseRequest.update_proj(name, usr, proj, function(err, x){
         if(err){
           console.log("problemi con l'update di un progetto");
