29cb846b6edb76f02f7399cf7fa4da522cb1242a
Firion711 <jacknapier.serious@gmail.com>
2017-08-27 22:41:54 +0200

    ADD commenti

--- Front-End/src/app/components/editor-container/components/editor/components/activity-menu/activity-menu.component.ts
+++ Front-End/src/app/components/editor-container/components/editor/components/activity-menu/activity-menu.component.ts
@@ -3,122 +3,198 @@ import { Component } from '@angular/core';
 import { MainEditorService } from '../../../../../../services/main-editor.service';
 import { ActivityService } from '../../services/activity.service';
 
+/**
+* This component manage the activity shapes
+*/
 @Component({
 	selector: 'activity-menu',
 	templateUrl: './activity-menu.component.html',
 	styleUrls: ['./activity-menu.component.css']
 })
 export class ActivityMenuComponent {
-
-	decisions = ['if', 'for', 'while'];
-	dec: string = '';
+  /**
+  * This value rappresent the type of decision
+  */
+  decisions = ['if', 'for', 'while'];
+  /**
+  * This value rappresent decision taken
+  */
+  dec: string = '';
+  /**
+  * This value rappresent the parameter list
+  */
 	params: string[];
-
-	operators = ['<', '<=', '>', '>=', '==', '!='];
+  /**
+  * This value rappresent the operation list
+  */
+  operators = ['<', '<=', '>', '>=', '==', '!='];
+  /**
+  * This value rappresent the type list
+  */
 	types = ['short', 'int', 'long', 'float', 'double', 'boolean', 'char', 'String'];
-
-	modPro: boolean = false;
+  /**
+  * This value rappresent the mode of make the body
+  */
+  modPro: boolean = false;
+  /**
+  * This is a string used to assign to a buttom in html DOM
+  */
 	mod: string = 'Cambia Modalità';
 
-
-	// Valori dichiarazione variabile
-	nomeVar: string = '';
-	tipoVar: string = '';
+  // Valori dichiarazione variabile
+  /**
+  * This value rappresent the varaible name
+  */
+  nomeVar: string = '';
+  /**
+  * This value rappresent the varaible type
+  */
+  tipoVar: string = '';
+  /**
+  * This value rappresent the varaible value
+  */
 	valVar: any = '';
 
-	// Valori if
-	va: string = '';
+  // Valori if
+  /**
+  * This value rappresent the varaible name of if statement
+  */
+  va: string = '';
+  /**
+  * This value rappresent the operation into if statement
+  */
 	operando: string = '';
 
-	// Valori for & while
-	nomeInd: string = '';
-	valInd: number = 0;
-	maxInd: number = 0;
+  // Valori for & while
+  /**
+  * This value rappresent the varaible name of for loop
+  */
+  nomeInd: string = '';
+  /**
+  * This value rappresent the varaible index of for loop
+  */
+  valInd: number = 0;
+  /**
+  * This value rappresent the varaible max index of for loop
+  */
+  maxInd: number = 0;
+  /**
+  * This value rappresent the varaible operation of for loop
+  */
 	op: string = '';
-
+  /**
+  * Make an instance of ActivityMenuComponent
+  * @param mainEditorService used to create a new instantiation of MainEditorService
+  * @param activityService used to create a new instantiation of ActivityService
+  */
 	constructor(private mainEditorService: MainEditorService,
 		private activityService: ActivityService) { }
-
-	enterClassMode() {
-		this.activityService.salvaMetodo();
-	}
-
-	generaCodice() {
-		this.activityService.generaCodice();
-	}
-
-	changeName(name: string) {
-		this.activityService.changeName(name);
-		$('#newName').val('');
-	}
-
-	isMain() {
-		if (this.activityService.getNameMethod() == 'main')
-			return true;
-		return false;
-	}
-
-	generaOp(corpo: string) {
-		corpo = corpo.trim();
-		this.activityService.addBody(corpo);
-	}
-
-	generaIf() {
-		if (!this.modPro) {
-			// let re = new RegExp('^[0-9]+| +');
-			// this.nomeInd.trim();
-			var code = this.va + ' ' + this.op + ' ' + this.operando;
-		} else {
-			var code = this.va;
-		}
-		this.activityService.setDecisione('if', code);
-		this.va = this.operando = this.op = '';
-	}
-
-	generaFor() {
-		if (!this.modPro) {
-			this.valInd = parseInt(this.valInd.toFixed(0));
-			this.maxInd = parseInt(this.maxInd.toFixed(0));
-			this.nomeInd.trim();
-			let re = new RegExp('^[0-9]+| +');
-			if (re.test(this.nomeInd))
-				alert("Il nome dell'indice non può contenere spazi o iniziare con un numero");
-			else if (this.nomeInd && this.valInd && this.maxInd)
-				var code = 'int ' + this.nomeInd + ' = ' + this.valInd + '; ' + this.nomeInd + ' ' + this.op +
-					' ' + this.maxInd + '; ' + this.nomeInd + '++';
-		} else
+    /**
+    * This function switch to class mode
+    */
+    enterClassMode() {
+      this.activityService.salvaMetodo();
+    }
+    /**
+    * This function generate code
+    */
+    generaCodice() {
+      this.activityService.generaCodice();
+    }
+    /**
+    * This function change the name with the new value
+    * @param name the new value
+    */
+    changeName(name: string) {
+      this.activityService.changeName(name);
+      $('#newName').val('');
+    }
+    /**
+    * This function check if this method is the main
+    */
+    isMain() {
+      if (this.activityService.getNameMethod() == 'main')
+        return true;
+      return false;
+    }
+    /**
+    * This function generate the operation code
+    * @param corpo string code
+    */
+    generaOp(corpo: string) {
+      corpo = corpo.trim();
+      this.activityService.addBody(corpo);
+    }
+    /**
+    * This function generate the if statement
+    */
+    generaIf() {
+      if (!this.modPro) {
+        // let re = new RegExp('^[0-9]+| +');
+        // this.nomeInd.trim();
+        var code = this.va + ' ' + this.op + ' ' + this.operando;
+      } else {
+        var code = this.va;
+      }
+      this.activityService.setDecisione('if', code);
+      this.va = this.operando = this.op = '';
+    }
+    /**
+    * This function generate the for loop
+    */
+    generaFor() {
+      if (!this.modPro) {
+        this.valInd = parseInt(this.valInd.toFixed(0));
+        this.maxInd = parseInt(this.maxInd.toFixed(0));
+        this.nomeInd.trim();
+        let re = new RegExp('^[0-9]+| +');
+        if (re.test(this.nomeInd))
+          alert("Il nome dell'indice non può contenere spazi o iniziare con un numero");
+        else if (this.nomeInd && this.valInd && this.maxInd)
+          var code = 'int ' + this.nomeInd + ' = ' + this.valInd + '; ' + this.nomeInd + ' ' + this.op +
+        ' ' + this.maxInd + '; ' + this.nomeInd + '++';
+      } else
 			var code = this.nomeInd;
-		this.activityService.setDecisione('for', code);
-		this.nomeInd = this.op = '';
-		this.valInd = this.maxInd = 0;
-	}
-
-	generaWhile() {
-		if (!this.modPro) {
-			var code = this.va + ' ' + this.op + ' ' + this.operando;
-		} else {
-			var code = this.va;
-		}
-		this.activityService.setDecisione('while', code);
-		this.va = this.operando = this.op = '';
-	}
-
-	setNomeVar(name: string) {
-		this.nomeVar = name;
-	}
-
-	isNumeric() {
-		if (this.tipoVar != 'String' && this.tipoVar != 'char' && this.tipoVar != 'boolean')
-			return true;
-		return false;
-	}
-
-	confezionaVar() {
-		switch (this.tipoVar) {
-			case 'int':
+      this.activityService.setDecisione('for', code);
+      this.nomeInd = this.op = '';
+      this.valInd = this.maxInd = 0;
+    }
+    /**
+    * This function generate the while loop
+    */
+    generaWhile() {
+      if (!this.modPro) {
+        var code = this.va + ' ' + this.op + ' ' + this.operando;
+      } else {
+        var code = this.va;
+      }
+      this.activityService.setDecisione('while', code);
+      this.va = this.operando = this.op = '';
+    }
+    /**
+    * This function change the variable name with the new value
+    * @param name new value
+    */
+    setNomeVar(name: string) {
+      this.nomeVar = name;
+    }
+    /**
+    * This function check if the string is numeric
+    */
+    isNumeric() {
+      if (this.tipoVar != 'String' && this.tipoVar != 'char' && this.tipoVar != 'boolean')
+        return true;
+      return false;
+    }
+    /**
+    * This function make the variable
+    */
+    confezionaVar() {
+      switch (this.tipoVar) {
+        case 'int':
 				this.valVar.toFixed();
 				break;
-			case 'short':
+        case 'short':
 				this.valVar.toFixed();
 				if (this.valVar > 32767) {
 					this.valVar = 32767;
@@ -129,126 +205,145 @@ export class ActivityMenuComponent {
 					this.valVar = -32768;
 				}
 				break;
-			case 'long':
+        case 'long':
 				this.valVar.toFixed();
 				break;
-			case 'String':
+        case 'String':
 				this.valVar = '"' + this.valVar + '"';
 				break;
-			case 'char':
+        case 'char':
 				this.valVar = "'" + this.valVar + "'";
 				break;
-		}
-	}
-
-	declareVar() {
-		if (!this.modPro) {
-			if (typeof (this.valVar) == 'string')
-				this.valVar = this.valVar.trim();
-			this.nomeVar = this.nomeVar.trim();
-			if (this.nomeVar && this.tipoVar) {
-				let re = new RegExp('^[0-9]+| +');
-				if (re.test(this.nomeVar))
-					alert('Il nome della variabile non può iniziare con un numero o contenere spazi al suo interno');
-				else {
-					if (this.valVar) {
-						this.confezionaVar();
-						var code = this.tipoVar + ' ' + this.nomeVar + ' = ' + this.valVar;
-					}
-					else
-						var code = this.tipoVar + ' ' + this.nomeVar;
-				}
-			}
-			else
-				alert('Tipo o/e nome della variabile assente/i');
-		} else {
-			var code = this.nomeVar;
-		}
-		if (code) {
-			this.activityService.modBody(code, true);
-			this.activityService.addLocalVar(this.activityService.getSelectedShapeId(), this.nomeVar);
-			this.tipoVar = '';
-			this.valVar = '';
-			this.nomeVar = '';
-		}
-	}
-
-	deleteVar(id: string) {
-		if (id) {
-			this.activityService.deleteVar(id);
-		}
-	}
-
-	toggleModPro() {
-		if (this.modPro) {
-			this.modPro = false;
-		} else {
-			this.modPro = true;
-		}
-	}
-
-	modificaIf() {
-		if (this.activityService.getShapeType() != 'if')
-			this.generaIf();
-		else {
-			if (!this.modPro)
-				var code = this.va + ' ' + this.op + ' ' + this.operando;
-			else
-				var code = this.va;
-			this.activityService.modBody(code, false);
-			this.va = this.operando = this.op = '';
-		}
-		console.log(this.activityService.getShapeType());
-	}
-
-	modificaFor() {
-		if (this.activityService.getShapeType() != 'for')
-			this.generaFor();
-		else {
-			if (!this.modPro) {
-				this.valInd = parseInt(this.valInd.toFixed(0));
-				this.maxInd = parseInt(this.maxInd.toFixed(0));
-				this.nomeInd.trim();
-				let re = new RegExp('^[0-9]+| +');
-				if (re.test(this.nomeInd))
-					alert("Il nome dell'indice non può contenere spazi o iniziare con un numero");
-				else if (this.nomeInd && this.valInd && this.maxInd)
-					var code = 'int ' + this.nomeInd + ' = ' + this.valInd + '; ' + this.nomeInd + ' ' + this.op + '; ' +
-						this.nomeInd + '++';
-			} else
+      }
+    }
+    /**
+    * This function make the variable
+    */
+    declareVar() {
+      if (!this.modPro) {
+        if (typeof (this.valVar) == 'string')
+          this.valVar = this.valVar.trim();
+        this.nomeVar = this.nomeVar.trim();
+        if (this.nomeVar && this.tipoVar) {
+          let re = new RegExp('^[0-9]+| +');
+          if (re.test(this.nomeVar))
+            alert('Il nome della variabile non può iniziare con un numero o contenere spazi al suo interno');
+          else {
+            if (this.valVar) {
+              this.confezionaVar();
+              var code = this.tipoVar + ' ' + this.nomeVar + ' = ' + this.valVar;
+            }
+            else
+              var code = this.tipoVar + ' ' + this.nomeVar;
+          }
+        }
+        else
+          alert('Tipo o/e nome della variabile assente/i');
+      } else {
+        var code = this.nomeVar;
+      }
+      if (code) {
+        this.activityService.modBody(code, true);
+        this.activityService.addLocalVar(this.activityService.getSelectedShapeId(), this.nomeVar);
+        this.tipoVar = '';
+        this.valVar = '';
+        this.nomeVar = '';
+      }
+    }
+    /**
+    * This function delete the variable selected by id
+    * @param id: id shape
+    */
+    deleteVar(id: string) {
+      if (id) {
+        this.activityService.deleteVar(id);
+      }
+    }
+    /**
+    * This funtion change the mode to edit a statement
+    */
+    toggleModPro() {
+      if (this.modPro) {
+        this.modPro = false;
+      } else {
+        this.modPro = true;
+      }
+    }
+    /**
+    * This function edit the if statement
+    */
+    modificaIf() {
+      if (this.activityService.getShapeType() != 'if')
+        this.generaIf();
+      else {
+        if (!this.modPro)
+          var code = this.va + ' ' + this.op + ' ' + this.operando;
+        else
+          var code = this.va;
+        this.activityService.modBody(code, false);
+        this.va = this.operando = this.op = '';
+      }
+      console.log(this.activityService.getShapeType());
+    }
+    /**
+    * This function edit the for loop
+    */
+    modificaFor() {
+      if (this.activityService.getShapeType() != 'for')
+        this.generaFor();
+      else {
+        if (!this.modPro) {
+          this.valInd = parseInt(this.valInd.toFixed(0));
+          this.maxInd = parseInt(this.maxInd.toFixed(0));
+          this.nomeInd.trim();
+          let re = new RegExp('^[0-9]+| +');
+          if (re.test(this.nomeInd))
+            alert("Il nome dell'indice non può contenere spazi o iniziare con un numero");
+          else if (this.nomeInd && this.valInd && this.maxInd)
+            var code = 'int ' + this.nomeInd + ' = ' + this.valInd + '; ' + this.nomeInd + ' ' + this.op + '; ' +
+          this.nomeInd + '++';
+        } else
 				var code = this.nomeInd;
-			this.activityService.modBody(code, false);
-		}
-		console.log(this.activityService.getShapeType());
-	}
+        this.activityService.modBody(code, false);
+      }
+      console.log(this.activityService.getShapeType());
+    }
+    /**
+    * This function edit the while loop
+    */
+    modificaWhile() {
+      if (this.activityService.getShapeType() != 'while')
+        this.generaWhile();
+      else {
+        if (!this.modPro)
+          var code = this.va + ' ' + this.op + ' ' + this.operando;
+        else
+          var code = this.va;
+        this.activityService.modBody(code, false);
+        this.va = this.operando = this.op = '';
+      }
+      console.log(this.activityService.getShapeType());
+    }
+    /**
+    * This function return the parameter list
+    */
+    getParams() {
+      return this.activityService.getParams();
+    }
+    /**
+    * This function delete the selected element
+    */
+    deleteElement() {
+      this.activityService.deleteElement();
+    }
+    /**
+    * This function check if the element is selected
+    */
+    isSelected() {
+      if (this.activityService.getSelectedShape() && this.activityService.getSelectedElement()) {
+        return true;
+      }
+      return false;
+    }
 
-	modificaWhile() {
-		if (this.activityService.getShapeType() != 'while')
-			this.generaWhile();
-		else {
-			if (!this.modPro)
-				var code = this.va + ' ' + this.op + ' ' + this.operando;
-			else
-				var code = this.va;
-			this.activityService.modBody(code, false);
-			this.va = this.operando = this.op = '';
-		}
-		console.log(this.activityService.getShapeType());
-	}
-
-	getParams() {
-		return this.activityService.getParams();
-	}
-
-	deleteElement() {
-		this.activityService.deleteElement();
-	}
-
-	isSelected() {
-		if (this.activityService.getSelectedShape() && this.activityService.getSelectedElement()) {
-			return true;
-		}
-		return false;
-	}
-
-}
+  }
